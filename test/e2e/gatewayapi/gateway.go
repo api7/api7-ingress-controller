19749881315e03bddfdc50261f041ae32e0cc6c2
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2024-09-25 22:20:41 +0530

    fix test

--- test/e2e/gatewayapi/gateway.go
+++ test/e2e/gatewayapi/gateway.go
@@ -226,7 +226,7 @@ spec:
 			assert.Nil(GinkgoT(), err, "list tls error")
 			assert.Len(GinkgoT(), tls, 1, "tls number not expect")
 			assert.Equal(GinkgoT(), Cert, tls[0].Cert, "tls cert not expect")
-			assert.Equal(GinkgoT(), []string{host}, tls[0].Snis)
+			assert.Equal(GinkgoT(), []string{host, "*.api6.com"}, tls[0].Snis)
 		})
 
 		Context("Gateway SSL with and without hostname", func() {
