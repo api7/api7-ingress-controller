040379a1749762689af7720946a1af6400aa5b2d
Ashish Tiwari <ashishjaitiwari15112000@gmail.com>
2024-09-26 13:55:06 +0530

    skip conformance test

--- test/conformance/conformance_test.go
+++ test/conformance/conformance_test.go
@@ -74,8 +74,7 @@ func TestGatewayAPIConformance(t *testing.T) {
 	opts.CleanupBaseResources = true
 	opts.GatewayClassName = gatewayClassName
 	opts.SupportedFeatures = sets.New(gatewaySupportedFeatures...)
-	opts.SkipTests = skippedTestsForSSL
-	opts.SkipTests = append(opts.SkipTests, skippedTestsForTraditionalRoutes...)
+	opts.SkipTests = append(skippedTestsForSSL, skippedTestsForTraditionalRoutes...)
 	opts.Implementation = conformancev1.Implementation{
 		Organization: "API7",
 		Project:      "api7-ingress-controller",
