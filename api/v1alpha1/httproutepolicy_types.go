00a8c6f45c650e3527138caa1e66ee88127761bb
悟空 <rainchan365@163.com>
2025-04-11 17:55:47 +0800

    Add HTTPRoutePolicy CRD and related configurations

--- api/v1alpha1/httproutepolicy_types.go
+++ api/v1alpha1/httproutepolicy_types.go
@@ -17,11 +17,10 @@ limitations under the License.
 package v1alpha1
 
 import (
-	"encoding/json"
-	"errors"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	gatewayv1alpha2 "sigs.k8s.io/gateway-api/apis/v1alpha2"
+
+	"github.com/api7/api7-ingress-controller/api/common"
 )
 
 // HTTPRoutePolicySpec defines the desired state of HTTPRoutePolicy.
@@ -47,7 +46,8 @@ type HTTPRoutePolicy struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
 
-	Spec HTTPRoutePolicySpec `json:"spec,omitempty"`
+	Spec   HTTPRoutePolicySpec          `json:"spec,omitempty"`
+	Status gatewayv1alpha2.PolicyStatus `json:"status,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -65,60 +65,8 @@ type HTTPRoutePolicySpecPolicy struct {
 }
 
 // Vars represents the route match expressions of APISIX.
-type Vars [][]StringOrSlice
-
-// UnmarshalJSON implements json.Unmarshaler interface.
-// lua-cjson doesn't distinguish empty array and table,
-// and by default empty array will be encoded as '{}'.
-// We have to maintain the compatibility.
-func (vars *Vars) UnmarshalJSON(p []byte) error {
-	if p[0] == '{' {
-		if len(p) != 2 {
-			return errors.New("unexpected non-empty object")
-		}
-		return nil
-	}
-	var data [][]StringOrSlice
-	if err := json.Unmarshal(p, &data); err != nil {
-		return err
-	}
-	*vars = data
-	return nil
-}
-
-// StringOrSlice represents a string or a string slice.
-// TODO Do not use interface{} to avoid the reflection overheads.
-type StringOrSlice struct {
-	StrVal   string   `json:"-"`
-	SliceVal []string `json:"-"`
-}
-
-func (s *StringOrSlice) MarshalJSON() ([]byte, error) {
-	var (
-		p   []byte
-		err error
-	)
-	if s.SliceVal != nil {
-		p, err = json.Marshal(s.SliceVal)
-	} else {
-		p, err = json.Marshal(s.StrVal)
-	}
-	return p, err
-}
-
-func (s *StringOrSlice) UnmarshalJSON(p []byte) error {
-	var err error
-
-	if len(p) == 0 {
-		return errors.New("empty object")
-	}
-	if p[0] == '[' {
-		err = json.Unmarshal(p, &s.SliceVal)
-	} else {
-		err = json.Unmarshal(p, &s.StrVal)
-	}
-	return err
-}
+// +kubebuilder:object:generate=false
+type Vars = common.Vars
 
 func init() {
 	SchemeBuilder.Register(&HTTPRoutePolicy{}, &HTTPRoutePolicyList{})
