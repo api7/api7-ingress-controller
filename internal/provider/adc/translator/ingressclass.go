7b9c258d4147cb5026c80ad63a287c06582f21a9
ashing <axingfly@gmail.com>
2025-04-27 12:00:18 +0800

    fix: r
    Signed-off-by: ashing <axingfly@gmail.com>

--- internal/provider/adc/translator/ingressclass.go
+++ internal/provider/adc/translator/ingressclass.go
@@ -1,11 +1,35 @@
 package translator
 
 import (
+	adctypes "github.com/api7/api7-ingress-controller/api/adc"
 	"github.com/api7/api7-ingress-controller/internal/provider"
+	"github.com/api7/gopkg/pkg/log"
+	"go.uber.org/zap"
 	networkingv1 "k8s.io/api/networking/v1"
 )
 
 func (t *Translator) TranslateIngressClass(tctx *provider.TranslateContext, obj *networkingv1.IngressClass) (*TranslateResult, error) {
 	result := &TranslateResult{}
+
+	rk := provider.ResourceKind{
+		Kind:      obj.Kind,
+		Namespace: obj.Namespace,
+		Name:      obj.Name,
+	}
+	gatewayProxy, ok := tctx.GatewayProxies[rk]
+	if !ok {
+		log.Debugw("no GatewayProxy found for IngressClass", zap.String("ingressclass", obj.Name))
+		return result, nil
+	}
+
+	globalRules := adctypes.Plugins{}
+	pluginMetadata := adctypes.Plugins{}
+	// apply plugins from GatewayProxy to global rules
+	t.fillPluginsFromGatewayProxy(globalRules, &gatewayProxy)
+	t.fillPluginMetadataFromGatewayProxy(pluginMetadata, &gatewayProxy)
+
+	result.GlobalRules = globalRules
+	result.PluginMetadata = pluginMetadata
+
 	return result, nil
 }
