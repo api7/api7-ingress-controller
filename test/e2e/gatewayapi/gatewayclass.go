443aa9ad9557c107d4c1691bd8a4a464598700b4
悟空 <rainchan365@163.com>
2025-04-29 08:51:01 +0800

    chore: Add GatewayClass protection and deletion logic
    Introduce a finalizer to prevent deletion of GatewayClasses still in use by Gateways, and implement corresponding e2e tests to validate the protection mechanism and proper removal flow.

--- test/e2e/gatewayapi/gatewayclass.go
+++ test/e2e/gatewayapi/gatewayclass.go
@@ -32,6 +32,18 @@ metadata:
 spec:
   controllerName: "apisix.apache.org/not-exist"
 `
+		const defaultGateway = `
+apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: api7ee
+spec:
+  gatewayClassName: api7
+  listeners:
+    - name: http1
+      protocol: HTTP
+      port: 80
+`
 		It("Create GatewayClass", func() {
 			By("create default GatewayClass")
 			err := s.CreateResourceFromStringWithNamespace(defautlGatewayClass, "")
@@ -52,6 +64,51 @@ spec:
 			Expect(err).NotTo(HaveOccurred(), "getting GatewayClass yaml")
 			Expect(gcyaml).To(ContainSubstring(`status: Unknown`), "checking GatewayClass condition status")
 			Expect(gcyaml).To(ContainSubstring("message: Waiting for controller"), "checking GatewayClass condition message")
+		})
+
+		It("Delete GatewayClass", func() {
+			By("create default GatewayClass")
+			err := s.CreateResourceFromStringWithNamespace(defautlGatewayClass, "")
+			Expect(err).NotTo(HaveOccurred(), "creating GatewayClass")
+			Eventually(func() string {
+				spec, err := s.GetResourceYaml("GatewayClass", "api7")
+				Expect(err).NotTo(HaveOccurred(), "get resource yaml")
+				return spec
+			}).WithTimeout(8 * time.Second).ProbeEvery(time.Second).Should(ContainSubstring(`status: "True"`))
+
+			By("create a Gateway")
+			err = s.CreateResourceFromString(defaultGateway)
+			Expect(err).NotTo(HaveOccurred(), "creating Gateway")
+			time.Sleep(time.Second)
+
+			By("try to delete the GatewayClass")
+			_, err = s.RunKubectlAndGetOutput("delete", "GatewayClass", "api7", "--wait=false")
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = s.GetResourceYaml("GatewayClass", "api7")
+			Expect(err).NotTo(HaveOccurred(), "get resource yaml")
+
+			output, err := s.RunKubectlAndGetOutput("describe", "GatewayClass", "api7")
+			Expect(err).NotTo(HaveOccurred(), "describe GatewayClass api7")
+			Expect(output).To(And(
+				ContainSubstring("Warning"),
+				ContainSubstring("DeletionBlocked"),
+				ContainSubstring("gatewayclass-controller"),
+				ContainSubstring("the GatewayClass is still in using by Gateways"),
+			))
+
+			By("delete the Gateway")
+			err = s.DeleteResource("Gateway", "api7ee")
+			Expect(err).NotTo(HaveOccurred(), "deleting Gateway")
+			time.Sleep(time.Second)
+
+			By("try to delete the GatewayClass again")
+			err = s.DeleteResource("GatewayClass", "api7")
+			Expect(err).NotTo(HaveOccurred())
+
+			_, err = s.GetResourceYaml("GatewayClass", "api7")
+			Expect(err).To(HaveOccurred(), "get resource yaml")
+			Expect(err.Error()).To(ContainSubstring("not found"))
 		})
 	})
 })
