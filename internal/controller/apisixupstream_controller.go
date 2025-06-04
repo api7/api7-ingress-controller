c2d88626a6579a12880f2bad736a7e97beb63fb6
悟空 <rainchan365@163.com>
2025-06-04 16:20:03 +0800

    feat: Add v2 API support for Apisix resources and update controller logic

---
+++ internal/controller/apisixupstream_controller.go
@@ -0,0 +1,63 @@
+/*
+Copyright 2024.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+
+	apisixapacheorgv2 "github.com/apache/apisix-ingress-controller/api/v2"
+)
+
+// ApisixUpstreamReconciler reconciles a ApisixUpstream object
+type ApisixUpstreamReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// +kubebuilder:rbac:groups=apisix.apache.org.github.com,resources=apisixupstreams,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=apisix.apache.org.github.com,resources=apisixupstreams/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=apisix.apache.org.github.com,resources=apisixupstreams/finalizers,verbs=update
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the ApisixUpstream object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.20.2/pkg/reconcile
+func (r *ApisixUpstreamReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	_ = log.FromContext(ctx)
+
+	// TODO(user): your logic here
+
+	return ctrl.Result{}, nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ApisixUpstreamReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&apisixapacheorgv2.ApisixUpstream{}).
+		Named("apisixupstream").
+		Complete(r)
+}
