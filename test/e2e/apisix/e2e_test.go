f3640023c964d3e9e3e527bd8d5b9acc714a9a31
rongxin <rongxin@api7.ai>
2025-06-10 13:33:38 +0800

    add ingress

--- test/e2e/apisix/e2e_test.go
+++ test/e2e/apisix/e2e_test.go
@@ -22,6 +22,7 @@ import (
 	_ "github.com/apache/apisix-ingress-controller/test/e2e/crds"
 	"github.com/apache/apisix-ingress-controller/test/e2e/framework"
 	_ "github.com/apache/apisix-ingress-controller/test/e2e/gatewayapi"
+	_ "github.com/apache/apisix-ingress-controller/test/e2e/ingress"
 	"github.com/apache/apisix-ingress-controller/test/e2e/scaffold"
 )
 
