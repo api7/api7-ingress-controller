8aa0dec14c00a4f6d085fbcca560a315d33600dc
rongxin <rongxin@api7.ai>
2025-06-19 17:55:13 +0800

    fix lint2

--- internal/provider/adc/translator/apisixconsumer.go
+++ internal/provider/adc/translator/apisixconsumer.go
@@ -1,9 +1,6 @@
-// Licensed to the Apache Software Foundation (ASF) under one or more
-// contributor license agreements.  See the NOTICE file distributed with
-// this work for additional information regarding copyright ownership.
-// The ASF licenses this file to You under the Apache License, Version 2.0
-// (the "License"); you may not use this file except in compliance with
-// the License.  You may obtain a copy of the License at
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
 //
 //	http://www.apache.org/licenses/LICENSE-2.0
 //
@@ -12,19 +9,21 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 package translator
 
 import (
 	"fmt"
 	"strconv"
 
+	"github.com/pkg/errors"
+	k8stypes "k8s.io/apimachinery/pkg/types"
+
 	adctypes "github.com/apache/apisix-ingress-controller/api/adc"
 	v2 "github.com/apache/apisix-ingress-controller/api/v2"
 	"github.com/apache/apisix-ingress-controller/internal/controller/label"
 	"github.com/apache/apisix-ingress-controller/internal/provider"
 	"github.com/apache/apisix-ingress-controller/internal/types"
-	"github.com/pkg/errors"
-	k8stypes "k8s.io/apimachinery/pkg/types"
 )
 
 var (
