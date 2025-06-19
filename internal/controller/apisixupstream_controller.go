2755d41e7559fdb36b54fd4aa1ff8d8d9e1273d0
悟空 <rainchan365@163.com>
2025-06-19 13:41:33 +0800

    feat: add apisixupstream controller (#169)

--- internal/controller/apisixupstream_controller.go
+++ internal/controller/apisixupstream_controller.go
@@ -13,8 +13,8 @@
 package controller
 
 import (
+	"cmp"
 	"context"
-	"fmt"
 
 	"github.com/go-logr/logr"
 	networkingv1 "k8s.io/api/networking/v1"
@@ -33,8 +33,8 @@ import (
 	"github.com/apache/apisix-ingress-controller/internal/utils"
 )
 
-// ApisixPluginConfigReconciler reconciles a ApisixPluginConfig object
-type ApisixPluginConfigReconciler struct {
+// ApisixUpstreamReconciler reconciles a ApisixUpstream object
+type ApisixUpstreamReconciler struct {
 	client.Client
 	Scheme  *runtime.Scheme
 	Log     logr.Logger
@@ -42,26 +42,26 @@ type ApisixPluginConfigReconciler struct {
 }
 
 // SetupWithManager sets up the controller with the Manager.
-func (r *ApisixPluginConfigReconciler) SetupWithManager(mgr ctrl.Manager) error {
+func (r *ApisixUpstreamReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&apiv2.ApisixPluginConfig{}).
+		For(&apiv2.ApisixUpstream{}).
 		WithEventFilter(predicate.GenerationChangedPredicate{}).
 		Watches(&networkingv1.IngressClass{},
-			handler.EnqueueRequestsFromMapFunc(r.listApisixPluginConfigForIngressClass),
+			handler.EnqueueRequestsFromMapFunc(r.listApisixUpstreamForIngressClass),
 			builder.WithPredicates(
 				predicate.NewPredicateFuncs(r.matchesIngressController),
 			),
 		).
 		Watches(&v1alpha1.GatewayProxy{},
-			handler.EnqueueRequestsFromMapFunc(r.listApisixPluginConfigForGatewayProxy),
+			handler.EnqueueRequestsFromMapFunc(r.listApisixUpstreamForGatewayProxy),
 		).
-		Named("apisixpluginconfig").
+		Named("apisixupstream").
 		Complete(r)
 }
 
-func (r *ApisixPluginConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	var pc apiv2.ApisixPluginConfig
-	if err := r.Get(ctx, req.NamespacedName, &pc); err != nil {
+func (r *ApisixUpstreamReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	var au apiv2.ApisixUpstream
+	if err := r.Get(ctx, req.NamespacedName, &au); err != nil {
 		return ctrl.Result{}, client.IgnoreNotFound(err)
 	}
 
@@ -70,30 +70,30 @@ func (r *ApisixPluginConfigReconciler) Reconcile(ctx context.Context, req ctrl.R
 		err error
 	)
 	defer func() {
-		r.updateStatus(&pc, err)
+		r.updateStatus(&au, err)
 	}()
 
-	if ic, err = r.getIngressClass(&pc); err != nil {
+	if ic, err = r.getIngressClass(&au); err != nil {
 		return ctrl.Result{}, err
 	}
-	if err = r.processIngressClassParameters(ctx, &pc, ic); err != nil {
+	if err = r.processIngressClassParameters(ctx, &au, ic); err != nil {
 		return ctrl.Result{}, err
 	}
 	return ctrl.Result{}, nil
 }
 
-func (r *ApisixPluginConfigReconciler) listApisixPluginConfigForIngressClass(ctx context.Context, object client.Object) (requests []reconcile.Request) {
+func (r *ApisixUpstreamReconciler) listApisixUpstreamForIngressClass(ctx context.Context, object client.Object) (requests []reconcile.Request) {
 	ic, ok := object.(*networkingv1.IngressClass)
 	if !ok {
 		return nil
 	}
 
 	isDefaultIngressClass := IsDefaultIngressClass(ic)
-	var pcList apiv2.ApisixPluginConfigList
-	if err := r.List(ctx, &pcList); err != nil {
+	var auList apiv2.ApisixUpstreamList
+	if err := r.List(ctx, &auList); err != nil {
 		return nil
 	}
-	for _, pc := range pcList.Items {
+	for _, pc := range auList.Items {
 		if pc.Spec.IngressClassName == ic.Name || (isDefaultIngressClass && pc.Spec.IngressClassName == "") {
 			requests = append(requests, reconcile.Request{NamespacedName: utils.NamespacedName(&pc)})
 		}
@@ -101,7 +101,7 @@ func (r *ApisixPluginConfigReconciler) listApisixPluginConfigForIngressClass(ctx
 	return requests
 }
 
-func (r *ApisixPluginConfigReconciler) listApisixPluginConfigForGatewayProxy(ctx context.Context, object client.Object) (requests []reconcile.Request) {
+func (r *ApisixUpstreamReconciler) listApisixUpstreamForGatewayProxy(ctx context.Context, object client.Object) (requests []reconcile.Request) {
 	gp, ok := object.(*v1alpha1.GatewayProxy)
 	if !ok {
 		return nil
@@ -114,13 +114,13 @@ func (r *ApisixPluginConfigReconciler) listApisixPluginConfigForGatewayProxy(ctx
 	}
 
 	for _, ic := range icList.Items {
-		requests = append(requests, r.listApisixPluginConfigForIngressClass(ctx, &ic)...)
+		requests = append(requests, r.listApisixUpstreamForIngressClass(ctx, &ic)...)
 	}
 
 	return requests
 }
 
-func (r *ApisixPluginConfigReconciler) matchesIngressController(obj client.Object) bool {
+func (r *ApisixUpstreamReconciler) matchesIngressController(obj client.Object) bool {
 	ingressClass, ok := obj.(*networkingv1.IngressClass)
 	if !ok {
 		return false
@@ -128,19 +128,40 @@ func (r *ApisixPluginConfigReconciler) matchesIngressController(obj client.Objec
 	return matchesController(ingressClass.Spec.Controller)
 }
 
-func (r *ApisixPluginConfigReconciler) getIngressClass(pc *apiv2.ApisixPluginConfig) (*networkingv1.IngressClass, error) {
-	if pc.Spec.IngressClassName == "" {
+func (r *ApisixUpstreamReconciler) getIngressClass(au *apiv2.ApisixUpstream) (*networkingv1.IngressClass, error) {
+	if au.Spec.IngressClassName == "" {
 		return r.getDefaultIngressClass()
 	}
 
 	var ic networkingv1.IngressClass
-	if err := r.Get(context.Background(), client.ObjectKey{Name: pc.Spec.IngressClassName}, &ic); err != nil {
+	if err := r.Get(context.Background(), client.ObjectKey{Name: au.Spec.IngressClassName}, &ic); err != nil {
 		return nil, err
 	}
 	return &ic, nil
 }
 
-func (r *ApisixPluginConfigReconciler) getDefaultIngressClass() (*networkingv1.IngressClass, error) {
+func (r *ApisixUpstreamReconciler) processIngressClassParameters(ctx context.Context, au *apiv2.ApisixUpstream, ic *networkingv1.IngressClass) error {
+	if ic == nil || ic.Spec.Parameters == nil {
+		return nil
+	}
+
+	var (
+		parameters = ic.Spec.Parameters
+	)
+	if parameters.APIGroup == nil || *parameters.APIGroup != v1alpha1.GroupVersion.Group || parameters.Kind != KindGatewayProxy {
+		return nil
+	}
+
+	// check if the parameters reference GatewayProxy
+	var (
+		gp v1alpha1.GatewayProxy
+		ns = cmp.Or(parameters.Namespace, &au.Namespace)
+	)
+
+	return r.Get(ctx, client.ObjectKey{Namespace: *ns, Name: parameters.Name}, &gp)
+}
+
+func (r *ApisixUpstreamReconciler) getDefaultIngressClass() (*networkingv1.IngressClass, error) {
 	var icList networkingv1.IngressClassList
 	if err := r.List(context.Background(), &icList); err != nil {
 		r.Log.Error(err, "failed to list ingress classes")
@@ -157,45 +178,15 @@ func (r *ApisixPluginConfigReconciler) getDefaultIngressClass() (*networkingv1.I
 	}
 }
 
-// processIngressClassParameters processes the IngressClass parameters that reference GatewayProxy
-func (r *ApisixPluginConfigReconciler) processIngressClassParameters(ctx context.Context, pc *apiv2.ApisixPluginConfig, ingressClass *networkingv1.IngressClass) error {
-	if ingressClass == nil || ingressClass.Spec.Parameters == nil {
-		return nil
-	}
-
-	var (
-		parameters = ingressClass.Spec.Parameters
-	)
-	if parameters.APIGroup == nil || *parameters.APIGroup != v1alpha1.GroupVersion.Group || parameters.Kind != KindGatewayProxy {
-		return nil
-	}
-
-	// check if the parameters reference GatewayProxy
-	var (
-		gatewayProxy v1alpha1.GatewayProxy
-		ns           = parameters.Namespace
-	)
-	if ns == nil {
-		ns = &pc.Namespace
-	}
-
-	return r.Get(ctx, client.ObjectKey{Namespace: *ns, Name: parameters.Name}, &gatewayProxy)
-}
-
-func (r *ApisixPluginConfigReconciler) updateStatus(pc *apiv2.ApisixPluginConfig, err error) {
-	SetApisixCRDConditionAccepted(&pc.Status, pc.GetGeneration(), err)
+func (r *ApisixUpstreamReconciler) updateStatus(au *apiv2.ApisixUpstream, err error) {
+	SetApisixCRDConditionAccepted(&au.Status, au.GetGeneration(), err)
 	r.Updater.Update(status.Update{
-		NamespacedName: utils.NamespacedName(pc),
-		Resource:       &apiv2.ApisixPluginConfig{},
+		NamespacedName: utils.NamespacedName(au),
+		Resource:       &apiv2.ApisixUpstream{},
 		Mutator: status.MutatorFunc(func(obj client.Object) client.Object {
-			cp, ok := obj.(*apiv2.ApisixPluginConfig)
-			if !ok {
-				err := fmt.Errorf("unsupported object type %T", obj)
-				panic(err)
-			}
-			cpCopy := cp.DeepCopy()
-			cpCopy.Status = pc.Status
-			return cpCopy
+			cp := obj.(*apiv2.ApisixUpstream).DeepCopy()
+			cp.Status = au.Status
+			return cp
 		}),
 	})
 }
