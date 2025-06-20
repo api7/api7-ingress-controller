b9e1aaeb0ce72f077aa63ae910261392e8387bfd
悟空 <rainchan365@163.com>
2025-06-20 16:37:27 +0800

    refactor: remove unused Pods map from provider struct

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -46,7 +46,6 @@ type TranslateContext struct {
 	PluginConfigs          map[k8stypes.NamespacedName]*v1alpha1.PluginConfig
 	ApisixPluginConfigs    map[k8stypes.NamespacedName]*apiv2.ApisixPluginConfig
 	Services               map[k8stypes.NamespacedName]*corev1.Service
-	Pods                   map[k8stypes.NamespacedName][]*corev1.Pod
 	BackendTrafficPolicies map[k8stypes.NamespacedName]*v1alpha1.BackendTrafficPolicy
 	Upstreams              map[k8stypes.NamespacedName]*apiv2.ApisixUpstream
 	GatewayProxies         map[types.NamespacedNameKind]v1alpha1.GatewayProxy
@@ -64,7 +63,6 @@ func NewDefaultTranslateContext(ctx context.Context) *TranslateContext {
 		PluginConfigs:          make(map[k8stypes.NamespacedName]*v1alpha1.PluginConfig),
 		ApisixPluginConfigs:    make(map[k8stypes.NamespacedName]*apiv2.ApisixPluginConfig),
 		Services:               make(map[k8stypes.NamespacedName]*corev1.Service),
-		Pods:                   make(map[k8stypes.NamespacedName][]*corev1.Pod),
 		BackendTrafficPolicies: make(map[k8stypes.NamespacedName]*v1alpha1.BackendTrafficPolicy),
 		Upstreams:              make(map[k8stypes.NamespacedName]*apiv2.ApisixUpstream),
 		GatewayProxies:         make(map[types.NamespacedNameKind]v1alpha1.GatewayProxy),
