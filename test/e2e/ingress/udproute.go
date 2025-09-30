34391a86da8eb8db36d05abca5237ab58f7fff8e
Ashing Zheng <axingfly@gmail.com>
2025-09-30 17:03:40 +0800

    feat: support udproute webhook (#2588)
    Signed-off-by: Ashing Zheng <axingfly@gmail.com>
    (cherry picked from commit 63c7d111a4fc4e557e570f34b2a452c9d4941bb2)

---
+++ test/e2e/ingress/udproute.go
@@ -0,0 +1,48 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package webhook
+
+import (
+	. "github.com/onsi/ginkgo/v2"
+
+	"github.com/apache/apisix-ingress-controller/test/e2e/scaffold"
+)
+
+var _ = Describe("Test UDPRoute Webhook", Label("webhook"), func() {
+	s := scaffold.NewScaffold(scaffold.Options{
+		Name:          "udproute-webhook-test",
+		EnableWebhook: true,
+	})
+
+	BeforeEach(func() {
+		setupSimpleGatewayWithProtocol(s, "UDP", "udp", 9000)
+	})
+
+	It("should warn on missing backend services", func() {
+		tc := simpleRouteWebhookTestCase{
+			routeKind:       "UDPRoute",
+			routeName:       "webhook-udproute",
+			sectionName:     "udp",
+			missingService:  "missing-udp-backend",
+			servicePortName: "udp",
+			servicePort:     53,
+			serviceProtocol: "UDP",
+		}
+		verifySimpleRouteMissingBackendWarnings(s, tc)
+	})
+})
