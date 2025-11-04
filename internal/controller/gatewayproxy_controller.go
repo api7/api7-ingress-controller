apache/apisix-ingress-controller

chore: unify the logging component (#2584)

internal/controller/gatewayproxy_controller.go
@@ -100,7 +100,7 @@ func (r *GatewayProxyController) Reconcile(ctx context.Context, req ctrl.Request
 	if providerService == nil {
 		tctx.EndpointSlices[req.NamespacedName] = nil
 	} else {
-		if err := addProviderEndpointsToTranslateContext(tctx, r.Client, types.NamespacedName{
+		if err := addProviderEndpointsToTranslateContext(tctx, r.Client, r.Log, types.NamespacedName{
 			Namespace: gp.Namespace,
 			Name:      providerService.Name,
 		}); err != nil {

api7/api7-ingress-controller

Signed-off-by: Ashing Zheng <axingfly@gmail.com>

internal/controller/gatewayproxy_controller.go
@@ -136,16 +136,10 @@ func (r *GatewayProxyController) Reconcile(ctx context.Context, req ctrl.Request
 	if providerService == nil {
 		tctx.EndpointSlices[req.NamespacedName] = nil
 	} else {
-		serviceNN := k8stypes.NamespacedName{
+		if err := addProviderEndpointsToTranslateContext(tctx, r.Client, r.Log, k8stypes.NamespacedName{
 			Namespace: gp.Namespace,
 			Name:      providerService.Name,
-		}
-		service := &corev1.Service{}
-		if err := r.Get(ctx, serviceNN, service); err != nil {
-			return reconcile.Result{}, err
-		}
-		tctx.Services[serviceNN] = service
-		if err := resolveServiceEndpoints(tctx, r.Client, serviceNN, r.supportsEndpointSlice, nil); err != nil {
+		}); err != nil {
 			return reconcile.Result{}, err
 		}
 	}